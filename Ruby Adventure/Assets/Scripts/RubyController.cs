0935937a47435338ad9fc2e9813f989e9a22f4b0
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2023-03-12 19:43:00 +0000

    SONIDO Y PARTICULAS

--- Ruby Adventure/Assets/Scripts/RubyController.cs
+++ Ruby Adventure/Assets/Scripts/RubyController.cs
@@ -77,7 +77,7 @@ public class RubyController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
-              /*  NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+               /* NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
                 if (character != null)
                 {
                     character.DisplayDialog();
